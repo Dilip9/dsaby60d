4df1d8b9fdff2057c39edf336557bce712ee49ea
Dilip9 <dilip.verma940805@gmail.com>
2025-08-27 13:14:54 +0530

    DSA by 65 Days start day -1

---
+++ DSA_B_60_D/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("***************Array based most frequently asked questions in hard and medium******************");
+    }
+}
---
+++ DSA_B_60_D/src/arrays/PgmPractice1.java
@@ -0,0 +1,99 @@
+package arrays;
+
+public class PgmPractice1 {
+    // move all zero at the starting end.
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,2,9,0,9,0,15,0,7,0};  // 1. problem move all zero at the end.
+        printArray(arr);
+        System.out.println("::::::::::::::::Before doing final operations::::::::::");
+       
+        pushZeroAtEnd(arr);
+        int[] zot = {1,0,2,0,1,1,0,2,0,1,0,0,1,1,2,2,1,1,0};
+        printArray(arr);
+        System.out.println("::::::::::::::::Before doing final operations::::::::::");
+        sortZeroOneTwo(zot);                    // 21. Sort zero, One and two in ascending order.
+        int[] maj = {1,4,6,2,7,3,4,5,3,4,2,3,4,3,4,4,3,4,4,4,2,4,4,4,2,3,4,4,4,3,4};
+        majorityElementInArray(maj);
+        removeDuplicateFromSortedArray();
+        BestTimeToBuyStock();
+        NumberOfZeroFilledSubArray();
+        IncreasingTrippletesSequence();
+        FirstMissingPositive();
+        SearchElementArray(arr);
+        FindMinNMaxArray(arr);
+
+
+
+    }
+    static void pushZeroAtEnd(int[] arr){
+        int counter = 0;
+        for(int n : arr){
+            if(n !=0 ){
+                arr[counter++] = n;
+            }
+        }
+        while (counter<arr.length) {
+            arr[counter++] = 0;
+            
+        }
+        printArray(arr);
+    }
+    static void sortZeroOneTwo(int[] arr){
+        int low = 0, mid=0, high=arr.length-1;
+        while(mid<=high){
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low++, mid++);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high--);
+                    break;
+            }
+        }
+        printArray(arr);
+    }
+    static void majorityElementInArray(int[] majority){
+        int count = 1, candidate = majority[0];
+        for(int i=1;i<majority.length;i++){
+            if(candidate == majority[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count==0){
+                candidate = majority[i];
+                count = 1;
+            }
+        }
+        count=0;
+        for(int n: majority){
+            if(candidate == n){
+                count++;
+            }
+        }if(count>majority.length/2){
+            System.out.println(candidate);
+        }else{
+            System.out.println("Nothing is majority");
+        }
+    }
+
+
+
+    static void swap(int[] arr, int low, int high){
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+    }
+
+    static void printArray(int[] arr){
+        for(int n: arr){
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+
+
+}
