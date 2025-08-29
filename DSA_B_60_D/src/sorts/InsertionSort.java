de18e52a19ae38ddf666436a24ffefdb65e02ab7
Dilip9 <dilip.verma940805@gmail.com>
2025-08-29 18:07:42 +0530

    Insertion sort

--- DSA_B_60_D/src/sorts/InsertionSort.java
+++ DSA_B_60_D/src/sorts/InsertionSort.java
@@ -7,11 +7,9 @@ public class InsertionSort {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1,2,4,6,7,3);
         insertionSortStepWise(nums);
+        int[] aray = {43,65,23,76,34,87,34,24,43,345,23,56,432,12,6,23,876,965,4,233,23,65,34,54,768,56,23}; 
+        insertionSort(aray);
     }
-
-
-
-
 
 
     public static void insertionSortStepWise(List<Integer> num){
@@ -27,10 +25,34 @@ public class InsertionSort {
         printArray(num);
     }
 
+    public static void insertionSort(int[] nums){
+        
+    for (int i = 1; i < nums.length; i++) {
+                int value = nums[i];
+                int j = i - 1;
+
+                while (j >= 0 && nums[j] > value) {
+                    nums[j + 1] = nums[j];
+                    j--;
+                }
+
+                nums[j + 1] = value;
+
+                System.out.println("Step " + i + ":");
+                printArrays(nums);
+            }
+
+    }
     public static void printArray(List<Integer> nums){
         for(int n : nums){
             System.out.print(n+" ");
         }
         System.out.println();
+    }
+
+    public static void printArrays(int[] n){
+        for(int nn: n){
+            System.out.print(nn+" ");
+        }
     }
 }
