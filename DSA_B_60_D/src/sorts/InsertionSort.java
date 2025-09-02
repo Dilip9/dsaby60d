b57e6f1d554eb659d14893fd4ee3668740df61bc
Dilip9 <dilip.verma940805@gmail.com>
2025-09-02 11:43:19 +0530

    Remove duplicate from an array without insertion preserve

--- DSA_B_60_D/src/sorts/InsertionSort.java
+++ DSA_B_60_D/src/sorts/InsertionSort.java
@@ -9,6 +9,10 @@ public class InsertionSort {
         insertionSortStepWise(nums);
         int[] aray = {43,65,23,76,34,87,34,24,43,345,23,56,432,12,6,23,876,965,4,233,23,65,34,54,768,56,23}; 
         insertionSort(aray);
+        // Remove duplicate element from an array also maintain insertion order preserved.
+        System.out.println();
+        System.out.println(" remove duplicate from an array...");
+        removeDuplicateFromSortedArray(aray);
     }
 
 
@@ -25,22 +29,32 @@ public class InsertionSort {
         printArray(num);
     }
 
+    public static void removeDuplicateFromSortedArray(int[] arr){
+        int j =0;
+        int n = arr.length;
+        for(int i = 1; i<n; i++){
+            if(arr[i] != arr[j]){
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+        printArrays(arr);
+    }
     public static void insertionSort(int[] nums){
         
-    for (int i = 1; i < nums.length; i++) {
-                int value = nums[i];
-                int j = i - 1;
-
-                while (j >= 0 && nums[j] > value) {
-                    nums[j + 1] = nums[j];
-                    j--;
-                }
-
-                nums[j + 1] = value;
-
-                System.out.println("Step " + i + ":");
-                printArrays(nums);
+        for (int i = 1; i < nums.length; i++) {
+            int value = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > value) {
+                nums[j + 1] = nums[j];
+                j--;
             }
+
+            nums[j + 1] = value;
+
+            System.out.println("Step " + i + ":");
+            printArrays(nums);
+        }
 
     }
     public static void printArray(List<Integer> nums){
