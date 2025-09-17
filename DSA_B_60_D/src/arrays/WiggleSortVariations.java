5bf5c9ad96d53f851f15bc6ac25635bd69de2902
Dilip9 <dilip.verma940805@gmail.com>
2025-09-17 18:19:20 +0530

    WiggleSort and Graph algo problem

--- DSA_B_60_D/src/arrays/WiggleSortVariations.java
+++ DSA_B_60_D/src/arrays/WiggleSortVariations.java
@@ -4,6 +4,7 @@ public class WiggleSortVariations {
     public static void main(String[] args) {
         int[] num = {3,5,2,1,6,4};
         wiggleSortI(num);
+        int[] nums = {1,5,1,1,6,4};
         wiggleSortII(nums);
     }
     public static void wiggleSortI(int[] num){
@@ -23,5 +24,41 @@ public class WiggleSortVariations {
             System.out.print(n+" ");
         }
         System.out.println(" ");
+    }
+
+    // Variations of wiggle-II
+    public static void wiggleSortII(int[] nums){
+        int median = findKthLarget(nums, (nums.length+1)/2);
+        int n = nums.length;
+        int i = 0, left = 0, right = n-1;
+        while(i<=right){
+            if(nums[newIndex(i,n)]>median){
+                swap(nums, newIndex(left++,n), newIndex(i++,n));
+            }else if(nums[newIndex(i,n)]<median){
+                swap(nums, newIndex(right--,n), newIndex(i,n));
+            }else{
+                i++;
+            }
+        }
+        show(nums);
+    }
+    private static int findKthLarget(int[] num, int k){
+        k = num.length-k;
+        int low= 0, high = num.length-1;
+        while(low<high){
+            final int j = partition(num, low, high);
+            if(j<k){
+                low = j+1;
+            }else if(j>k){
+                high = j -1;
+            }else{
+                break;
+            }
+        }
+        return num[k];
+    }
+
+    private static int partition(int[] nums, int left, int right){
+        
     }
 }
