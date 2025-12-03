4f0ce415d709a13530f77c0ae7899875ba709ebd
Dilip9 <dilip.verma940805@gmail.com>
2025-12-03 23:59:50 +0530

    Variable and Window Shrinking

--- DSA_B_60_D/src/sliding_w_p/Window_Shrinking.java
+++ DSA_B_60_D/src/sliding_w_p/Window_Shrinking.java
@@ -1,5 +1,40 @@
-public class Window_Shrinking {
-    public static void main(){
+public class Window_Shrinking{
+    public static void main(String[] args){
 
+        /**
+         * Minimum Window Substring (Shrinking Window) Classic Leetcode hard Problem
+         *  **/
+        minWindow("ADOBECODEBANC", "ABC");
     }
+
+    public static void minWindow(String s, String t){
+        int[] freq = new int[128];
+        for(char c : t.toCharArray()){
+            freq[c]++;
+        }
+        int required = t.length();
+        int left = 0, minLength = Integer.MAX_VALUE, start =0;
+        for(int right =0; right < s.length(); right++){
+            char c = s.charAt(right);
+            if(freq[c]-- > 0){
+                required--;
+            }
+
+            while(required == 0){
+                if(right - left + 1 < minLength){
+                    minLength = right - left + 1;
+                    start = left;
+                }
+                if(++freq[s.charAt(left)] > 0){
+                    required++;
+                }
+                left++;
+            }
+        }
+        //return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
+        System.out.println(minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength));
+    }
+
+
+
 }
