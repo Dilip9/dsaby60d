31e3ee043b6040e9cf8a0709669046b15dcc0201
Dilip9 <dilip.verma940805@gmail.com>
2026-03-25 23:58:23 +0530

    Basic of recursion problem

--- DSA_B_60_D/src/recursion/SumOfN.java
+++ DSA_B_60_D/src/recursion/SumOfN.java
@@ -1,4 +1,17 @@
 package recursion;
 
 public class SumOfN {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = sumOfN(n);
+        System.out.println("The sum of the first " + n + " natural numbers is: " + result);
+    }
+    public static int sumOfN(int n){
+
+        if(n==1){
+            return 1;
+        }
+        return n + sumOfN(n-1);
+    }
 }
