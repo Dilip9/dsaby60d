4a9487f844d0f72770f95f1db1fbaf2aee53d1b7
Dilip9 <dilip.verma940805@gmail.com>
2026-03-26 00:10:37 +0530

    Brute force approach for sumofn

--- DSA_B_60_D/src/recursion/SumOfN.java
+++ DSA_B_60_D/src/recursion/SumOfN.java
@@ -5,6 +5,8 @@ public class SumOfN {
     public static void main(String[] args) {
         int n = 5;
         int result = sumOfN(n);
+        int resultIterative = sumOfNIterative(n);
+        System.out.println("Bruteforce approach:"+resultIterative);
         System.out.println("The sum of the first " + n + " natural numbers is: " + result);
     }
     public static int sumOfN(int n){
@@ -13,5 +15,12 @@ public class SumOfN {
             return 1;
         }
         return n + sumOfN(n-1);
+    }
+    public static int sumOfNIterative(int n){
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum += i;
+        }
+        return sum;
     }
 }
